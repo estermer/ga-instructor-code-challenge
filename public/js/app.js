af9cb756e43e1067f8eba5af45b7c5fde8a77886
estermer <eric.stermer@gmail.com>
2016-10-13 17:10:34 -0400

    Added HTTP request and a variable to get the search query

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,21 @@
 window.onload = function(){
 
+  //Added AJAX request object
+  var xhttp;
+  //this conditional statement is to account for older browsers
+  if (window.XMLHttpRequest) {
+    xhttp = new XMLHttpRequest();
+    } else {
+    // code for IE6, IE5
+    xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+
   //create search button to add an event listener on it
   var searchButton = document.getElementById('search-button');
 
   var searchCallback = function(){
-
+    //value of searched option
+    var query = document.getElementsByName('search-movies')[0].value;
   };
 
   searchButton.addEventListener('click', searchCallback);
