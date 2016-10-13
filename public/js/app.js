ff9d4e5131e23ca1e2b561622cf9b05b95fa8322
estermer <eric.stermer@gmail.com>
2016-10-13 17:26:35 -0400

    add an AJAX request and logged the response

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,10 @@ window.onload = function(){
 
   //Added AJAX request object
   var xhttp;
+
+  //create search button to add an event listener on it
+  var searchButton = document.getElementById('search-button');
+
   //this conditional statement is to account for older browsers
   if (window.XMLHttpRequest) {
     xhttp = new XMLHttpRequest();
@@ -10,13 +14,24 @@ window.onload = function(){
     xhttp = new ActiveXObject("Microsoft.XMLHTTP");
   }
 
-  //create search button to add an event listener on it
-  var searchButton = document.getElementById('search-button');
 
+  //search event listener callback
   var searchCallback = function(){
+    
     //value of searched option
     var query = document.getElementsByName('search-movies')[0].value;
+
+    //send the request to the OMDbapi
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseText);
+      }
+    };
+    xhttp.open('GET', 'http://www.omdbapi.com/?t=' + query + '&y=&plot=short&r=json', true)
+    xhttp.send();
+
   };
 
+  //event listener when user hits search button
   searchButton.addEventListener('click', searchCallback);
 };
