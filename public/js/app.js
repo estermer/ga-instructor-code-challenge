f1fab999cf61dd147f7a9e14865ebbae1733addb
estermer <eric.stermer@gmail.com>
2016-10-13 20:54:02 -0400

    Added a fvorite button to enable adding it to the data store

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,10 @@ window.onload = function(){
 
   //create search button to add an event listener on it
   var searchButton = document.getElementById('search-button');
+  var favoriteButton = document.getElementById('favorite-button');
+
+  //variable for the currently displayed movies data
+  var currentlyDisplayedMovie = {};
 
   //this conditional statement is to account for older browsers
   if (window.XMLHttpRequest) {
@@ -14,31 +18,65 @@ window.onload = function(){
     xhttp = new ActiveXObject("Microsoft.XMLHTTP");
   }
 
-  //create display for the movies
-  var displayMovies = function(movie){
-    
+  ///create display for the movies///
+  var displayMovie = function(movie){
+
+    //clear any previous search
+    document.getElementById('movie-display').innerHTML = '';
+
+    //create movie card element
+    var movieCard = document.createElement('div');
+    movieCard.setAttribute('class', 'movie');
+    var title = '<h2 class="movie-title">' + movie.Title + '</h2><br>';
+    var release = '<h2 class="movie-release">Released: ' + movie.Released + '</h2>';
+    var rating = '<h2 class="movie-rating">Rating: ' + movie.Rated + '</h2>';
+    var poster = '<img src="' + movie.Poster + '"><br>';
+    var genre = '<h3 class="movie-genre"> Genre: ' + movie.Genre + '</h3>';
+    var director = '<h3 class="movie-director"> Directed By: ' + movie.Director + '</h3>';
+
+    //add the movieCard all together
+    movieCard.innerHTML = title + release + rating + poster + director + genre;
+
+    //append moviecard to the display
+    document.getElementById('movie-display').appendChild(movieCard);
+
   };
 
 
-  //search event listener callback
+  ///favorite event listener callback///
+  var favoriteCallback = function(){
+    console.log("I work");
+  };
+
+
+  ///search event listener callback///
   var searchCallback = function(){
 
     //value of searched option
-    var query = document.getElementsByName('search-movies')[0].value;
+    var query = document.getElementsByName('search-movies')[0];
 
     //send the request to the OMDbapi
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        console.log(this.response.Title);
-        displayMovies(this.response);
+        //diplay the movie to the DOM
+        displayMovie(this.response);
+        currentlyDisplayedMovie = this.response;
       }
     };
-    xhttp.open('GET', 'http://www.omdbapi.com/?t=' + query + '&y=&plot=short&r=json', true);
+    xhttp.open('GET', 'http://www.omdbapi.com/?t=' + query.value + '&y=&plot=short&r=json', true);
     xhttp.responseType = 'json';
     xhttp.send();
 
+    //clear search field
+    query.value = '';
+
+    //display favorite button
+    favoriteButton.style.display = 'inline-block';
   };
+
 
   //event listener when user hits search button
   searchButton.addEventListener('click', searchCallback);
+  //event listener for favorite button
+  favoriteButton.addEventListener('click', favoriteCallback);
 };
